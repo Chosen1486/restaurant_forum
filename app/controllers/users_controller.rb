6943642d539e33fcbd0a0cc98e5164c7ee37a3b1
Chosen <qq4868@hotmail.com>
2018-01-28 23:42:23 +0800

    show commented restaurant on user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
 
   def show
-    @user = User.find(params[:id]) 
+    @user = User.find(params[:id])
+    @commented_restaurants = @user.restaurants.uniq 
   end
 
   def edit
