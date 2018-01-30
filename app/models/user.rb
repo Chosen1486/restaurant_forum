80a493f4269e22dc5184ff305ab0ebb735fa2942
Chosen <qq4868@hotmail.com>
2018-01-30 23:44:44 +0800

    add dependency between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :comments
   has_many :restaurants, through: :comments
+  has_many :comments, dependent: :restrict_with_error
   mount_uploader :avatar, AvatarUploader
 
   def admin?
