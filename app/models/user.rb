bacbf4373efaa4aaa861496e79f9ef3dbcf50346
Chosen <qq4868@hotmail.com>
2018-01-31 00:11:39 +0800

    setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :restaurants, through: :comments
   has_many :comments, dependent: :restrict_with_error
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  
   mount_uploader :avatar, AvatarUploader
 
   def admin?
